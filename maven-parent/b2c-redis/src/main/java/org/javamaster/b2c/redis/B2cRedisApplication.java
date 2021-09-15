6d1f8a03dd4acfa07ecf231d67c83365bbde10ce
liangyudong <liangyudong@bluemoon.com.cn>
2021-09-15 19:11:14 +0800

    完善练习

--- maven-parent/b2c-redis/src/main/java/org/javamaster/b2c/redis/B2cRedisApplication.java
+++ maven-parent/b2c-redis/src/main/java/org/javamaster/b2c/redis/B2cRedisApplication.java
@@ -2,12 +2,13 @@ package org.javamaster.b2c.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
 /**
  * @author yudong
  * @date 2020/9/15
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisAutoConfiguration.class)
 public class B2cRedisApplication {
 
     public static void main(String[] args) {
